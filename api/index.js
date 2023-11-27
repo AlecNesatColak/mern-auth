662fc53a6a6059c6ad998547a42e017274c6ab75
Alec-Nesat Colak <acolak9@ufl.edu>
2023-11-27 14:30:10 -0500

    Added API routes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
@@ -18,3 +19,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+app.use('/api/user', userRoutes);
