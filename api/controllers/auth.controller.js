afa76bb171b12097bfb5a68049f572fa2d1fd453
Alec-Nesat Colak <acolak9@ufl.edu>
2023-11-28 10:34:37 -0500

    added a middleware and error function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
   const salt = await bcrypt.genSalt(10);
@@ -12,6 +13,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "Signup success!" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
